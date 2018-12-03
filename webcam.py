0a3754c7b13e68c428f8b09a3817728bf59c7f42
naruka1023 <naruka1023@yahoo.com>
2018-12-03 22:08:24 +0700

    first Commit

---
+++ webcam.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+from threading import Thread
+  
+class Webcam:
+  
+    def __init__(self):
+
+        self.video_capture = cv2.VideoCapture(0)
+        time.sleep(2)
+        self.current_frame = self.video_capture.read()[1]
+          
+    # create thread for capturing images
+    def start(self):
+        Thread(target=self._update_frame, args=()).start()
+  
+    def _update_frame(self):
+        while(True):
+            self.current_frame = self.video_capture.read()[1]
+                  
+    # get the current frame
+    def get_current_frame(self):
+        return self.current_frame
